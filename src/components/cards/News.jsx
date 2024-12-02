77172f64d9071dce1d800d0e3436a78dfb22aec0
dohodyaga <nnnzagora625@mail.ru>
2024-12-02 14:04:36 +0000

    fix: исправление кнопок, стилей

--- src/components/cards/News.jsx
+++ src/components/cards/News.jsx
@@ -5,15 +5,15 @@ import { Text } from "@consta/uikit/Text";
 const NewsCard = ({ title, description, date }) => {
   return (
     <div style={{ flex: '1 1 calc(50% - 5px)', margin: '2.5px' }}>
-      <Card style={{ minHeight: "150px", width: "100%", position: 'relative', padding: "15px" }}>
+      <Card style={{ minHeight: "150px", width: "100%", padding: "15px" }}>
         <Layout direction="column" style={{ height: '100%' }}>
           <Text weight="bold" style={{ marginBottom: '10px' }}>{title}</Text>
           <Text weight="regular" style={{ flexGrow: 1 }}>{description}</Text>
-          <Text weight="regular" style={{ position: 'absolute', bottom: '10px', right: '10px' }}>{date}</Text>
+          <Text weight="regular" style={{ marginTop: '10px', alignSelf: 'flex-end' }}>{date}</Text>
         </Layout>
       </Card>
     </div>
   );
-};
+}
 
 export default NewsCard;
